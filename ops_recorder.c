85ef8c5a62f93ba5e78d2406dcbbf924120eb633
atan94 <amostan94@gmail.com>
2025-12-23 20:33:34 +0800

    add ops_recorder and push_swap header files with operation management functions

---
+++ ops_recorder.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ops_recorder.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 19:55:40 by amtan             #+#    #+#             */
+/*   Updated: 2025/12/23 19:55:53 by amtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+static int	ops_write_op(int fd, t_op op)
+{
+	if (op == OP_SA)
+		return (write(fd, "sa\n", 3) == 3);
+	if (op == OP_SB)
+		return (write(fd, "sb\n", 3) == 3);
+	if (op == OP_SS)
+		return (write(fd, "ss\n", 3) == 3);
+	if (op == OP_PA)
+		return (write(fd, "pa\n", 3) == 3);
+	if (op == OP_PB)
+		return (write(fd, "pb\n", 3) == 3);
+	if (op == OP_RA)
+		return (write(fd, "ra\n", 3) == 3);
+	if (op == OP_RB)
+		return (write(fd, "rb\n", 3) == 3);
+	if (op == OP_RR)
+		return (write(fd, "rr\n", 3) == 3);
+	if (op == OP_RRA)
+		return (write(fd, "rra\n", 4) == 4);
+	if (op == OP_RRB)
+		return (write(fd, "rrb\n", 4) == 4);
+	if (op == OP_RRR)
+		return (write(fd, "rrr\n", 4) == 4);
+	return (0);
+}
+
+int	ops_init(t_ops *ops)
+{
+	if (!ops)
+		return (0);
+	ops->buf = (uint8_t *)malloc(OPS_INIT_CAP);
+	if (!ops->buf)
+		return (0);
+	ops->len = 0;
+	ops->cap = OPS_INIT_CAP;
+	return (1);
+}
+
+void	ops_free(t_ops *ops)
+{
+	if (!ops)
+		return ;
+	free(ops->buf);
+	ops->buf = NULL;
+	ops->len = 0;
+	ops->cap = 0;
+}
+
+int	ops_append(t_ops *ops, t_op op)
+{
+	uint8_t	*new_buf;
+	size_t	new_cap;
+
+	if (!ops)
+		return (0);
+	if (ops->len >= ops->cap)
+	{
+		new_cap = ops->cap;
+		if (new_cap == 0)
+			new_cap = OPS_INIT_CAP;
+		while (new_cap <= ops->len)
+			new_cap *= 2;
+		new_buf = (uint8_t *)malloc(new_cap);
+		if (!new_buf)
+			return (0);
+		if (ops->buf && ops->len > 0)
+			ft_memcpy(new_buf, ops->buf, ops->len);
+		free(ops->buf);
+		ops->buf = new_buf;
+		ops->cap = new_cap;
+	}
+	ops->buf[ops->len] = (uint8_t)op;
+	ops->len += 1;
+	return (1);
+}
+
+int	ops_print_fd(const t_ops *ops, int fd)
+{
+	size_t	i;
+
+	if (!ops || fd < 0)
+		return (0);
+	i = 0;
+	while (i < ops->len)
+	{
+		if (!ops_write_op(fd, (t_op)ops->buf[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
---
+++ push_swap.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/23 19:52:48 by amtan             #+#    #+#             */
+/*   Updated: 2025/12/23 19:55:10 by amtan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stddef.h>
+# include <stdint.h>
+
+# define OPS_INIT_CAP 16384
+
+typedef enum e_op
+{
+	OP_SA = 0,
+	OP_SB = 1,
+	OP_SS = 2,
+	OP_PA = 3,
+	OP_PB = 4,
+	OP_RA = 5,
+	OP_RB = 6,
+	OP_RR = 7,
+	OP_RRA = 8,
+	OP_RRB = 9,
+	OP_RRR = 10
+}	t_op;
+
+typedef struct s_ops
+{
+	uint8_t	*buf;
+	size_t	len;
+	size_t	cap;
+}	t_ops;
+
+int		ops_init(t_ops *ops);
+void	ops_free(t_ops *ops);
+int		ops_append(t_ops *ops, t_op op);
+int		ops_print_fd(const t_ops *ops, int fd);
+
+#endif
