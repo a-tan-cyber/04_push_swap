b473301b650025dacdb161d78adf2398208a1486
atan94 <amostan94@gmail.com>
2025-12-27 18:43:44 +0800

    fix(Makefile): update source files list to include get_next_line and its utilities

--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -1,32 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoull.c                                        :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/04 12:00:00 by amtan             #+#    #+#             */
-/*   Updated: 2025/12/04 13:36:56 by amtan            ###   ########.fr       */
+/*   Created: 2025/12/06 13:54:06 by amtan             #+#    #+#             */
+/*   Updated: 2025/12/27 18:24:54 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-unsigned long long	ft_atoull(const char *nptr)
-{
-	unsigned long long	result;
+# include <stdlib.h>
+# include <unistd.h>
 
-	result = 0;
-	while ((*nptr >= '\t' && *nptr <= '\r') || *nptr == ' ')
-		nptr++;
-	if (*nptr == '+')
-		nptr++;
-	if (*nptr == '-')
-		return (0);
-	while (*nptr >= '0' && *nptr <= '9')
-	{
-		result = result * 10 + (unsigned long long)(*nptr - '0');
-		nptr++;
-	}
-	return (result);
-}
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*free_and_ret_null(char *stash);
+size_t	gnl_strlen(char *s);
+char	*gnl_strjoin(char *stash, char *buffer);
+char	*gnl_strchr(char *s, char c);
+
+#endif
